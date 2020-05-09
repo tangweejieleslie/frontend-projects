ac7d8128bb87a9c2d583b589c994a9af287f6e5e
Leslie Tang <tangweejieleslie+github@gmail.com>
2020-05-09 22:09:16 +0800

    experiment with proxy + added gitignore

--- ccg/src/helpers/proxy.js
+++ ccg/src/helpers/proxy.js
@@ -1,16 +1,46 @@
-let httpProxy = require('http-proxy');
-let http = require('http');
+// let httpProxy = require("http-proxy");
+// let http = require("http");
 
-//
-// Create your proxy server and set the target in the options.
-//
-let proxy = httpProxy.createProxyServer({target:'http://localhost:9000'}).listen(8000);
+// //
+// // Create your proxy server and set the target in the options.
+// //
+// let proxy = httpProxy
+//   .createProxyServer({ target: "http://localhost:9000" })
+//   .listen(8000);
 
-//
-// Create your target server
-//
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.write('request successfully proxied!' + '\n' + JSON.stringify(req.headers, true, 2));
-    res.end();
-  }).listen(9000);
+// //
+// // Create your target server
+// //
+// http
+//   .createServer(function (req, res) {
+//     res.writeHead(200, { "Content-Type": "text/plain" });
+//     res.write(
+//       "request successfully proxied!" +
+//         "\n" +
+//         JSON.stringify(req.headers, true, 2)
+//     );
+//     res.end();
+//   })
+//   .listen(9000);
+
+var Proxy = require("http-mitm-proxy");
+var proxy = Proxy();
+
+proxy.onError(function (ctx, err) {
+  console.error("proxy error:", err);
+});
+
+proxy.onRequest(function (ctx, callback) {
+  ctx.use(Proxy.gunzip);
+
+  ctx.onResponseData(function (ctx, chunk, callback) {
+    chunk = new Buffer(
+      chunk.toString().replace(/<h3.*?<\/h3>/g, "<h3>Pwned!</h3>")
+    );
+    return callback(null, chunk);
+  });
+
+  return callback();
+});
+
+proxy.listen({ port: 8081 });
